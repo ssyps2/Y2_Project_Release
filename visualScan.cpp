0faef4c5f5553382ecba6d5e45e6421d46954b49
pengyuan shu <ssyps2@nottingham.edu.cn>
2022-03-31 14:27:08 +0800

    feat: update the visual related part

--- visualScan.cpp
+++ visualScan.cpp
@@ -1,5 +1,4 @@
 #include "visualScan.hpp"
-//#include "opencv_aee.hpp"
 
 
 /* Prototype of Functions */
@@ -10,6 +9,9 @@ static void on_low_S_thresh_trackbar(int, void *);
 static void on_high_S_thresh_trackbar(int, void *);
 static void on_low_V_thresh_trackbar(int, void *);
 static void on_high_V_thresh_trackbar(int, void *);
+Mat transformPerspective(std::vector<Point> boundingContour, Mat frame, int x_size, int y_size);
+Point findContourCentre(std::vector<cv::Point> contour);
+int getMaxAreaContourId(Vector<Vector<cv::Point>> contours);
 static void resizeCamera(vehicleControl_t *robot, int width, int height);
 static void modeCheck(vehicleControl_t *robot);
 static void RGBCallback(int event, int x, int y, int, void *param);
@@ -17,8 +19,8 @@ static void HSVCallback(int event, int x, int y, int, void *param);
 
 extern vehicleControl_t robot;
 
-int matching_frame_width = 480, matching_frame_height = 360;
-int tracking_frame_width = 480, tracking_frame_height = 272;
+const int matching_frame_width = 320, matching_frame_height = 240;
+const int tracking_frame_width = 480, tracking_frame_height = 272;
 string comparePath = "/home/pi/Pictures/Template/CountShape2.png";
 
 Mat imageMatch, imageTrack;
@@ -26,14 +28,10 @@ Mat imageCompareInput, imageCompare;
 Mat imageHSV, imageMask, imageErode, imageDilate;
 Mat imgHSVCompare, imageMaskCompare;
 Mat transformMatrix, transformImage, imageXOR;
-Mat kernelErode, kernelDilate;
 
-//double contour_area;
 float compare_output;
 
-Vector <Vector<Point>> contours, contoursCompare;
-Vector <Vector<Point>> approx, approxCompare;
-Point2f input_coordinate[4], output_coordinate[4];
+Point2f output_coordinate[4];
 
 Mat gray_image, pinkChannel, pinkMask;
 int last_tx;
@@ -200,31 +198,31 @@ int midPointCapture(vehicleControl_t *robot){
     setMouseCallback("Processed Video", RGBCallback, &imageTrack);
 
     //display the processed video
-    imshow("Processed Video", imageTrack);
+    //imshow("Processed Video", imageTrack);
     //imshow("Binary", gray_image);
     //imshow("pink channel", pinkMask);
-    waitKey(1);
+    //waitKey(1);
 
     return tx;
 }
 
 float visualMatch(vehicleControl_t *robot) {
-    compare_output = 0;
     robot->webcam.read(imageMatch);
 
     imageCompareInput = imread(comparePath);
     resize(imageCompareInput, imageCompare, Size(matching_frame_width, matching_frame_height));
 
-    char keyRecord = (char) waitKey(1);
+    /*char keyRecord = (char) waitKey(1);
     if (keyRecord == 'c') {
         imwrite("/home/pi/Pictures/record.png", imageMatch);
         std::cout << "Record success" << std::endl;
-    }
+    }*/
 
     //trackBarHsvDetection(robot);
 
-    kernelErode = getStructuringElement(MORPH_RECT, Size(6, 6));
-    kernelDilate = getStructuringElement(MORPH_RECT, Size(3, 3));
+    Mat kernelErode = getStructuringElement(MORPH_RECT, Size(8, 8));
+    Mat kernelDilate = getStructuringElement(MORPH_RECT, Size(6, 6));
+    Mat kernelDilate2 = getStructuringElement(MORPH_RECT, Size(3, 3));
 
     cvtColor(imageMatch, imageHSV, COLOR_BGR2HSV);
     inRange(imageHSV, Scalar(157, 157, 114), Scalar(180, 255, 255), imageMask);
@@ -233,44 +231,81 @@ float visualMatch(vehicleControl_t *robot) {
     inRange(imgHSVCompare, Scalar(64, 0, 0), Scalar(180, 255, 255), imageMaskCompare);
 
     //erode-dilate algorithm
+    dilate(imageMask, imageMask, kernelDilate);
     erode(imageMask, imageErode, kernelErode);
-    dilate(imageErode, imageDilate, kernelDilate);
-    findContours(imageDilate, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    dilate(imageErode, imageDilate, kernelDilate2);
 
-//    contour_area = contourArea(contours[0]);
+    findContours(imageDilate, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    int contourID = getMaxAreaContourId(contours);
+    Vector<Point> boundingContour = contours[contourID];
 
-    /*approx.resize(contours.size());
-
-    for (int i = 0; i < contours.size(); i++) {
-        approx[i].resize(contours[i].size());
-        approxPolyDP(contours[i], approx[i], 5, true);
+    Vector<Point> approx;
+    approxPolyDP(boundingContour, approx, 5, true);
+    Point point1{0,0}, point2{0,0}, point3{0,0}, point4{0,0};
+    int point1_cnt = 0, point2_cnt = 0, point3_cnt = 0, point4_cnt = 0;
+    Point contourCentre = findContourCentre(boundingContour);
+//    circle(imageMatch, contourCentre, 4, Scalar(0,0,255),4);
+//    imshow("what", imageMatch);
+//    waitKey(0);
+    for (int i = 0; i<approx.size(); i++){
+        if(approx[i].x < contourCentre.x){
+            if(approx[i].y < contourCentre.y){
+                point1.x += approx[i].x;
+                point1.y += approx[i].y;
+                point1_cnt++;
+            }
+            else{
+                point4.x += approx[i].x;
+                point4.y += approx[i].y;
+                point4_cnt++;
+            }
+        }
+        else if (approx[i].x >= contourCentre.x){
+            if(approx[i].y < contourCentre.y){
+                point2.x += approx[i].x;
+                point2.y += approx[i].y;
+                point2_cnt++;
+            }
+            else{
+                point3.x += approx[i].x;
+                point3.y += approx[i].y;
+                point3_cnt++;
+            }
+        }
     }
-    drawContours(imageMatch, Mat(approx[0]), -1, Scalar(0, 255, 0), 8);
 
-    //find contours of compared image
-    findContours(imageMaskCompare, contoursCompare, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    point1.x /= point1_cnt; point1.y /= point1_cnt;
+    point2.x /= point2_cnt; point2.y /= point2_cnt;
+    point3.x /= point3_cnt; point3.y /= point3_cnt;
+    point4.x /= point4_cnt; point4.y /= point4_cnt;
 
-    approxCompare.resize(contoursCompare.size());
+    Vector<Point> inputCorner = {point1, point2, point3, point4};
 
-    for (int i = 0; i < contoursCompare.size(); i++) {
-        approxCompare[i].resize(contoursCompare[i].size());
-        approxPolyDP(contoursCompare[i], approxCompare[i], 5, true);
+    drawContours(imageMatch, Mat(inputCorner), -1, Scalar(0,255,0), 8);
+//    imshow("what", imageMatch);
+//    waitKey(0);
+
+    findContours(imageMaskCompare, contoursCompare, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    int contourCompareID = getMaxAreaContourId(contoursCompare);
+    Vector<Point> boundingContourCompare = contoursCompare[contourCompareID];
+
+    Vector<Point> approxCompare;
+    approxPolyDP(boundingContourCompare, approxCompare, 5, true);
+    drawContours(imageCompare, Mat(approxCompare), -1, Scalar(0,255,0), 8);
+
+    for (int j = 0; j < 4; j++){
+        output_coordinate[j] = approxCompare[j];
     }
-    drawContours(imageCompare, Mat(approxCompare[0]), -1, Scalar(0, 255, 0), 8);
+//
+//    transformMatrix = getPerspectiveTransform(input_coordinate, output_coordinate);
+//    warpPerspective(imageDilate, transformImage, transformMatrix, Size(imageDilate.cols,imageDilate.rows));
 
-
-    for (int j = 0; j < 4; j++) {
-        input_coordinate[j] = approx[0][j];
-        output_coordinate[j] = approxCompare[0][j];
-    }
-
-    transformMatrix = getPerspectiveTransform(input_coordinate, output_coordinate);
-    warpPerspective(imageDilate, transformImage, transformMatrix, Size(imageDilate.cols, imageDilate.rows));
+    transformImage = transformPerspective(inputCorner, imageDilate, matching_frame_width, matching_frame_height);
 
     //XOR
     bitwise_xor(transformImage, imageMaskCompare, imageXOR);
 
-    compare_output = 100.0f * (1 - (float) countNonZero(imageXOR) / (float) (imageXOR.cols * imageXOR.rows));*/
+    compare_output = 100.0f * (1 - (float) countNonZero(imageXOR) / (float) (imageXOR.cols * imageXOR.rows));
 
     //modeCheck(robot);
 
@@ -289,6 +324,175 @@ float visualMatch(vehicleControl_t *robot) {
     waitKey(1);
 
     return compare_output;
+}
+
+int getMaxAreaContourId(Vector<Vector<cv::Point>> contours){
+    double maxArea = 0;
+    int maxAreaContourId = -1;
+    for (int j = 0; j < contours.size(); j++) {
+        double newArea = cv::contourArea(contours.at(j));
+        if (newArea > maxArea) {
+            maxArea = newArea;
+            maxAreaContourId = j;
+        }
+    }
+    return maxAreaContourId;
+}
+
+Mat transformPerspective(std::vector<Point> boundingContour, Mat frame, int x_size, int y_size)
+{
+    if(boundingContour.size() != 4)
+    {
+        // Error the contour has too many points. Only 4 are allowed
+        Mat emptyMat;
+        return emptyMat;
+    }
+
+    Mat symbol(y_size,x_size,CV_8UC1, Scalar(0));
+
+    cv::Point2f symbolCorners[4], boundingCorners[4];      // Create (and populate) variables containing the corner locations for the transform
+    symbolCorners[0] = Point2f(0,0);
+    symbolCorners[1] = Point2f(symbol.cols - 1,0);
+    symbolCorners[2] = Point2f(symbol.cols - 1,symbol.rows - 1);
+    symbolCorners[3] = Point2f(0,symbol.rows - 1);
+
+    Point contourCentre = findContourCentre(boundingContour);   // To populate the contour corners we need to check the order of the points
+
+    int point1, point2, point3, point4;
+
+    if(boundingContour[0].x > contourCentre.x)
+    {
+        if(boundingContour[0].y > contourCentre.y)
+            point3 = 0;
+        else
+            point2 = 0;
+    }
+    else
+    {
+        if(boundingContour[0].y > contourCentre.y)
+            point4 = 0;
+        else
+            point1 = 0;
+    }
+
+    if(boundingContour[1].x > contourCentre.x)
+    {
+        if(boundingContour[1].y > contourCentre.y)
+            point3 = 1;
+        else
+            point2 = 1;
+    }
+    else
+    {
+        if(boundingContour[1].y > contourCentre.y)
+            point4 = 1;
+        else
+            point1 = 1;
+    }
+
+    if(boundingContour[2].x > contourCentre.x)
+    {
+        if(boundingContour[2].y > contourCentre.y)
+            point3 = 2;
+        else
+            point2 = 2;
+    }
+    else
+    {
+        if(boundingContour[2].y > contourCentre.y)
+            point4 = 2;
+        else
+            point1 = 2;
+    }
+
+    if(boundingContour[3].x > contourCentre.x)
+    {
+        if(boundingContour[3].y > contourCentre.y)
+            point3 = 3;
+        else
+            point2 = 3;
+    }
+    else
+    {
+        if(boundingContour[3].y > contourCentre.y)
+            point4 = 3;
+        else
+            point1 = 3;
+    }
+
+    if(point1 + point2 + point3 + point4 != 6)
+    {
+        //Unable to reconstruct rectangle
+        Mat emptyMat;
+        return emptyMat;
+    }
+
+/*
+    boundingCorners[0] = boundingContour[point1];
+    boundingCorners[1] = boundingContour[point2];
+    boundingCorners[2] = boundingContour[point3];
+    boundingCorners[3] = boundingContour[point4];
+*/
+    int dia_proc = 1; //with dilate pre-process
+    int dia_size = 0;
+
+    if (dia_proc)
+        dia_size = 4;
+
+    //Bonding the contour with dilate pre-process
+    boundingCorners[0].x = boundingContour[point1].x+dia_size;
+    boundingCorners[0].y = boundingContour[point1].y+dia_size;
+    boundingCorners[1].x = boundingContour[point2].x-dia_size;
+    boundingCorners[1].y = boundingContour[point2].y+dia_size;
+    boundingCorners[2].x = boundingContour[point3].x-dia_size;
+    boundingCorners[2].y = boundingContour[point3].y-dia_size;
+    boundingCorners[3].x = boundingContour[point4].x+dia_size;
+    boundingCorners[3].y = boundingContour[point4].y-dia_size;
+
+    Mat transformMatrix = cv::getPerspectiveTransform(boundingCorners, symbolCorners); // Calculate the required transform operation
+    Mat transformedSymbol(240,320,CV_8UC1,Scalar(0));
+    cv::warpPerspective(frame, transformedSymbol, transformMatrix, cv::Size(symbol.cols,symbol.rows));  // Perform the transformation
+
+    return transformedSymbol;
+}
+
+Point findContourCentre(std::vector<cv::Point> contour)
+{
+    Moments foundRegion;    // Variables to store the region moment and the centre point
+    Point centre;
+    foundRegion = moments(contour, false);      // Calculate the moment for the contour
+    centre.x = (foundRegion.m10/foundRegion.m00);  //Calculate the X and Y positions
+    centre.y = (foundRegion.m01/foundRegion.m00);
+
+    return centre;
+}
+
+/* Declaration of Functions */
+void TrackBar_HSV_Detection(const string& file_path){
+    Mat frame, frame_HSV, frame_threshold;
+    namedWindow(hsv_window_name);
+    // Trackbars to set thresholds for HSV values
+    createTrackbar("Low H", hsv_window_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
+    createTrackbar("High H", hsv_window_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
+    createTrackbar("Low S", hsv_window_name, &low_S, max_value, on_low_S_thresh_trackbar);
+    createTrackbar("High S", hsv_window_name, &high_S, max_value, on_high_S_thresh_trackbar);
+    createTrackbar("Low V", hsv_window_name, &low_V, max_value, on_low_V_thresh_trackbar);
+    createTrackbar("High V", hsv_window_name, &high_V, max_value, on_high_V_thresh_trackbar);
+
+    while (true) {
+        frame = imread(file_path);
+        // Convert from BGR to HSV colorspace
+        cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
+        // Detect the object based on HSV Range Values
+        inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+        // Show the frames
+        imshow(hsv_window_name, frame_threshold);
+        char key = (char) waitKey(30);
+        if (key == 'q' || key == 27)
+        {
+            break;
+        }
+    }
 }
 
 static void modeCheck(vehicleControl_t *robot){
